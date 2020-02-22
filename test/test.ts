3c93735294ff1a22814100187e59d5e05751084e
lintao <lintao_alex@163.com>
2020-02-22 14:09:00 +0800

    supply for test code

---
+++ test/test.ts
@@ -0,0 +1,46 @@
+import { Node } from '../src/core/Node'
+
+// the help function that simulate you action of request async data
+function createPms(...args: any[]) {
+    return new Promise<any>(resolve => {
+        setTimeout(() => {
+            console.log('resolve ', args)
+            resolve(args)
+        }, Math.random() * 3000 + 1000)
+    })
+}
+
+// create every Node you need by giving them then function which return a Promise
+let a = new Node(() => {
+    return createPms('a')
+})
+// only b will be shown again if you recall "c.pms"
+let b = new Node(() => {
+    return createPms('b')
+}, false)
+let b2 = new Node(() => {
+    return createPms('b2')
+})
+// the last one show the args pass by it's dependence. because you may request it by the prev response
+let c = new Node(createPms)
+
+Node.DEFAULT_CACHE = false
+// after this, all new Node will not cache if you don't set it's cache to true
+let d = new Node(() => {
+    return createPms('d')
+})
+let d2 = new Node(() => {
+    return createPms('d2')
+})
+
+
+// allocate the relationship
+b.depend(a)
+c.depend(b, b2)
+b2.depend(d, d2)
+
+// call the one you need
+c.pms
+
+console.log('waiting to see')
+export { a, b, b2, c }
